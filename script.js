4b621c98fe370206fda7737071e2460e579ab8e6
Alokdey07 <alokdeykv@gmail.com>
2022-06-23 03:31:15 +0530

    alok2

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const mealDetailsContent = document.querySelector('.meal-detials-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 //event listners
+
+searchBtn.addEventListener('click' , getMealList);
+
+//get meal list that matches with the ingredients
+function getMealList(){
+    let SearchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`www.themealdb.com/api/json/v1/1/filter.php?i=chicken_breast`).then(response => response.json()).then(data => {
+        console.log(data);
+    })
+    
+}
