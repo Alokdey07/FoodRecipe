38ba8d7e856236db82c1bf25584c9793acbe3c57
Alokdey07 <alokdeykv@gmail.com>
2022-06-25 22:07:51 +0530

    alok final

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const searchBtn = document.getElementById('search-btn');
 const mealList = document.getElementById('meal');
-const mealDetailsContent = document.querySelector('.meal-detials-content');
+const mealDetailsContent = document.querySelector('.meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 //event listners
 
 searchBtn.addEventListener('click', getMealList);
 mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click',() =>{mealDetailsContent.parentElement.classList.remove('showRecipe')})
 
 //get meal list that matches with the ingredients
 function getMealList() {
@@ -68,17 +69,18 @@ function mealRecipeModal(meal) {
             <p class="recipe-category">${meal.strCategory}</p>
             <div class="recipe-instruct">
                 <h3>Instructions:</h3>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, labore odit hic accusamus atque voluptatum nam placeat nulla excepturi natus earum eligendi! Magni nam voluptatem quasi perspiciatis saepe excepturi dolore.</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum ab harum architecto dicta quaerat nemo tempore natus recusandae consequatur minima?</p>
+                <p>${meal.strInstructions}</p>
             </div>
             <div class = "recipe-meal-img">
-                <img src="food.jpg" alt="">
+                <img src="${meal.strMealThumb}" alt="">
             </div>
             <div class="recipe-link">
-                <a href="#" target="_blank">Watch Video</a>
+                <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
             </div>
     
     `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
 
 }
 
